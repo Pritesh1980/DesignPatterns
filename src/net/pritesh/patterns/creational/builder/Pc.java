07d0779fdb69cdc17ae29162a8abc354b71b5d87
Pritesh <me@pritesh.net>
2016-07-07 19:47:17 +0100

    Added examples for the Builder pattern using a Pc.Builder

---
+++ src/net/pritesh/patterns/creational/builder/Pc.java
@@ -0,0 +1,85 @@
+package net.pritesh.patterns.creational.builder;
+
+/**
+ * An example of the Builder pattern.
+ * This builds a {@link Pc} by using a static inner {@link Builder} class to do the dirty work.
+ * Any compulsory fields could be added to the {@link Builder} constructor.
+ */
+public class Pc
+{
+    public static class Builder
+    {
+        private String cpu;
+        private String memory;
+        private String graphics;
+        private String psu;
+
+        public Builder()
+        {
+        }
+
+        public Pc build()
+        {
+            return new Pc(this);
+        }
+
+        public Builder cpu( String cpu )
+        {
+            this.cpu = cpu;
+            return this;
+        }
+
+        public Builder memory( String memory )
+        {
+            this.memory = memory;
+            return this;
+        }
+
+        public Builder graphics(String graphics)
+        {
+            this.graphics = graphics;
+            return this;
+        }
+
+        public Builder psu(String psu)
+        {
+            this.psu = psu;
+
+            return this;
+        }
+
+    }
+
+    private final String cpu;
+    private final String memory;
+    private final String graphics;
+    private final String psu;
+
+    public Pc( Builder builder )
+    {
+        this.cpu = builder.cpu;
+        this.memory = builder.memory;
+        this.graphics = builder.graphics;
+        this.psu = builder.psu;
+    }
+
+    public String getCpu()
+    {
+        return cpu;
+    }
+
+    public String getMemory()
+    {
+        return memory;
+    }
+
+    public String getGraphics()
+    {
+        return graphics;
+    }
+
+    public String getPsu()
+    {
+        return psu;
+    }
+}
---
+++ src/net/pritesh/patterns/creational/builder/PcTest.java
@@ -0,0 +1,31 @@
+package net.pritesh.patterns.creational.builder;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class PcTest
+{
+    @Test
+    public void buildTest() throws Exception
+    {
+        // Given
+        final String CPU = "Intel";
+        final String MEMORY = "32GB";
+        final String GRAPHICS = "Nvidia M60";
+        final String PSU = "750W";
+
+        Pc.Builder builder = new Pc.Builder();
+
+        // When
+        builder.cpu(CPU).memory(MEMORY).graphics(GRAPHICS).psu(PSU);
+        Pc pc = builder.build();
+
+        // Then
+        assertEquals(pc.getCpu(), CPU);
+        assertEquals(pc.getMemory(), MEMORY);
+        assertEquals(pc.getGraphics(), GRAPHICS);
+        assertEquals(pc.getPsu(), PSU);
+    }
+}
